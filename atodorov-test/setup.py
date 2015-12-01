d51ea19d2a100ddedfc4dbd022eb90be337c83ee
Alexander Todorov <atodorov@redhat.com>
2015-12-01 23:39:18 +0200

    Upstream fixes in 0.3.3, waiting for OpenShift to trigger

--- atodorov-test/setup.py
+++ atodorov-test/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 config = {
     'name' : 'atodorov-test',
-    'version' : '0.3.2',
+    'version' : '0.3.3',
     'packages' : find_packages(),
     'author' : 'Alexander Todorov',
     'author_email' : 'atodorov@nopam.otb.bg',
